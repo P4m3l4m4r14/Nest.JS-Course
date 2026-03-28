8524f855c42aeba3edb2f9b752f745bcf973aab8
P4m3l4m4r14 <pamelamaria@alu.ufc.br>
2026-03-28 16:57:20 -0300

    feat:create prisma migrate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
+import { DatabeseService } from './database/databese.service';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, DatabaseModule],
+  providers: [DatabeseService],
 })
 export class AppModule {}
